bb58cab7805cfbe78254609669206f025a1afe9b
MilouMohamed <49353852+MilouMohamed@users.noreply.github.com>
2023-02-10 09:48:13 +0000

    Add files via upload

---
+++ med/MAZAGAN TENTE NOMADE.js
@@ -0,0 +1,55 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "MAZAGAN TENTE NOMADE_controls.js"
+        }
+    ],
+"logo": {
+        "href": "",
+        "src": "PANORAMASTUDIO LOGO.PNG",
+        "style": "max-width: 80%;",
+        "target": "_blank"
+    },
+"node": {
+	"autoplay": {
+	        "pan": 4,
+	        "restart": 5,
+	        "status": "on",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 360,
+	        "maxtilt": 90,
+	        "minpan": 0,
+	        "mintilt": -90
+	    },
+	"id": "MAZAGAN TENTE NOMADE",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_1b_%y_%x.jpg"},"down":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_1d_%y_%x.jpg"},"front":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_1f_%y_%x.jpg"},"height":3183,"left":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_1l_%y_%x.jpg"},"right":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_1r_%y_%x.jpg"},"tilesize":480,"up":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_1u_%y_%x.jpg"},"width":3183},{"back":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_2b_%y_%x.jpg"},"down":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_2d_%y_%x.jpg"},"front":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_2f_%y_%x.jpg"},"height":1592,"left":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_2l_%y_%x.jpg"},"right":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_2r_%y_%x.jpg"},"tilesize":480,"up":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_2u_%y_%x.jpg"},"width":1592},{"back":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_3b_%y_%x.jpg"},"down":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_3d_%y_%x.jpg"},"front":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_3f_%y_%x.jpg"},"height":796,"left":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_3l_%y_%x.jpg"},"right":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_3r_%y_%x.jpg"},"tilesize":480,"up":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_3u_%y_%x.jpg"},"width":796},{"back":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_4b_%y_%x.jpg"},"down":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_4d_%y_%x.jpg"},"front":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_4f_%y_%x.jpg"},"height":398,"left":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_4l_%y_%x.jpg"},"right":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_4r_%y_%x.jpg"},"tilesize":480,"up":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_t_4u_%y_%x.jpg"},"width":398}],"multilevel":true,"preview":{"src":"MAZAGAN TENTE NOMADE_tiles/MAZAGAN TENTE NOMADE_preview.jpg"},"projection":"cubic"},
+	"textbox": [
+	        {
+	            "align": "top",
+	            "style": " color:#ffffff; font-family: Arial,Helvetica,sans-serif; text-shadow: 0px 0px 3.8px #ff0000, 0px 0px 2.5px #ff0000, 0px 0px 1.7px #ff0000; font-size: 12px; font-weight: bold; text-align:center;",
+	            "text": "INCENTIVE HOUSE [titre d'evenment]",
+	            "yoff": "4px"
+	        }
+	    ],
+	"view": {
+	        "hfov": 90,
+	        "pan": 180,
+	        "tilt": 0
+	    }
+},
+"version": "4.1"
+}
+}
